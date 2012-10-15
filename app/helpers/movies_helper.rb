b8cb6c6fcda02e6df0b8d95429ab1bfcd950d8e6
asteriskgr <asteriskgr@yahoo.gr>
2012-10-15 10:18:45 -0700

    Initial commit

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,7 +2,8 @@ module MoviesHelper
      def oddness(count)
         count.odd? ?  "odd" :  "even"
      end
-      
+  
+    
   
     def is_checked?(rating, selected_ratings)
         selected_ratings.include? rating
